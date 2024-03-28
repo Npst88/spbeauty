87e5c94168062eea90946f19f76bc24c438cd0f3
Npst88 <165124039+Npst88@users.noreply.github.com>
2024-03-28 23:01:59 +0000

    list

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -6,7 +6,7 @@ function Addresses() {
         <div className='d-inline-flex flex-column gap-2 mt-4 border rounded-3 p-4 justify-content-between col'>
             <div>
                 <div className='title'>Адреса заведений</div>
-                <div className='d-inline-flex flex-column gap-3'>
+                <div className='d-inline-flex flex-column gap-2 mt-4'>
                     <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                     <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 </div>
